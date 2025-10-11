6e3c27e3a3ba54924263d62f8cd5c717471ff5d8
bethibande <bethibande@gmail.com>
2025-10-11 12:33:50 +0200

    ByteBuffer backed implementation #7

--- core/src/main/java/de/bethibande/memory/Allocator.java
+++ core/src/main/java/de/bethibande/memory/Allocator.java
@@ -1,12 +1,17 @@
 package de.bethibande.memory;
 
 import de.bethibande.memory.impl.DirectAllocator;
+import de.bethibande.memory.impl.DirectNioAllocator;
 import de.bethibande.memory.impl.PooledAllocator;
 
 public interface Allocator {
 
     static Allocator direct() {
         return new DirectAllocator();
+    }
+
+    static Allocator directNio() {
+        return new DirectNioAllocator();
     }
 
     static PooledAllocator pooled(final int size) {
